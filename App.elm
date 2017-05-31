35b4c47d75fdef622da96748378f44f6782a3399
Luke Winikates <luke.winikates@gmail.com>
2017-05-31 06:23:08 -0700

    replace the index.html with the elm project

--- App.elm
+++ App.elm
@@ -3,6 +3,33 @@ module App exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import List exposing (take)
+
+type Orientation = Horizontal | Vertical
+
+type alias Movie =
+    { fileName : String,
+     orientation: Orientation}
+
+
+movies : List Movie
+movies =
+    [ { fileName = "IMG_6212.m4v", orientation = Vertical }
+    , { fileName = "IMG_6213.m4v", orientation = Vertical }
+    , { fileName = "IMG_6214.m4v", orientation = Vertical }
+    , { fileName = "IMG_6219.m4v", orientation = Vertical }
+    , { fileName = "IMG_6227.m4v", orientation = Horizontal }
+    , { fileName = "IMG_6230.m4v", orientation = Horizontal }
+    , { fileName = "IMG_6231.m4v", orientation = Vertical }
+    , { fileName = "IMG_6244.m4v", orientation = Vertical }
+    , { fileName = "IMG_6250.m4v", orientation = Vertical }
+    , { fileName = "IMG_6256.m4v", orientation = Vertical }
+    , { fileName = "IMG_6258.m4v", orientation = Horizontal }
+    , { fileName = "IMG_6259.m4v", orientation = Vertical }
+    , { fileName = "IMG_6260.m4v", orientation = Vertical }
+    , { fileName = "IMG_6263.m4v", orientation = Horizontal }
+    , { fileName = "IMG_6270.m4v", orientation = Vertical }
+    ]
 
 
 main : Program Never Model Msg
@@ -14,33 +41,42 @@ main =
         , subscriptions = subscriptions
         }
 
+type Scale = Large | Medium | Small
+
+type alias PositionedMovie =
+    { movie : Movie,
+      scale : Scale }
 
 type alias Model =
-    {  }
+    {}
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {
-      }
+    ( {}
     , Cmd.none
     )
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+subscriptions model =
+    Sub.none
 
 
-type Msg = Msg
+type Msg
+    = Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update action model = (model, Cmd.none)
+update action model =
+    ( model, Cmd.none )
 
+movieView : Movie -> Html Msg
+movieView movie =
+  video [(loop True), (height 50), (src ("/public/" ++movie.fileName)), (autoplay  True)] []
 
 view : Model -> Html Msg
 view model =
     div
-        [
-        ]
         []
+        (List.map movieView (take 6 movies))
---
+++ README.md
@@ -0,0 +1,2 @@
+to recompile: `elm make App.elm && http-server .`
+(elm reactor doesn't serve the videos from the public directory)
