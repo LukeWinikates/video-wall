0fb64078166ce6c41bf99fd18fa88e878a1e1b4f
Luke Winikates <luke.winikates@gmail.com>
2017-06-02 09:57:55 -0700

    introduce the idea of the frame as the strategy for organizing the layout.

--- App.elm
+++ App.elm
@@ -1,15 +1,21 @@
 module App exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html exposing (Html, div, video)
+import Html.Attributes exposing (autoplay, height, loop, src, style)
 import Html.Events exposing (..)
-import List exposing (take)
+import List exposing (head, map, tail, take)
+import List.Extra exposing (zip)
 
-type Orientation = Horizontal | Vertical
+
+type Orientation
+    = Horizontal
+    | Vertical
+
 
 type alias Movie =
-    { fileName : String,
-     orientation: Orientation}
+    { fileName : String
+    , orientation : Orientation
+    }
 
 
 movies : List Movie
@@ -32,35 +38,70 @@ movies =
     ]
 
 
+type Scale
+    = Small
+    | Medium
+    | Large
+
+
+type alias Layout =
+    { frames : List Frame, movies : List Movie }
+
+
+frames =
+    [ { orientation = Vertical, scale = Large }
+    , { orientation = Horizontal, scale = Medium }
+    , { orientation = Horizontal, scale = Medium }
+    , { orientation = Vertical, scale = Large }
+    ]
+
+
+
+--layout = a list of frames
+-- frame = an orientation
+
+
+type alias Frame =
+    { orientation : Orientation, scale : Scale }
+
+
+
+-- size Vertical Small = 1,2
+-- size Vertical Medium = 2,4
+-- size Vertical Large = 4,8
+-- size Horizontal Small = 2,1
+-- size Horizontal Medium = 4,2
+-- size Horizontal Large = 8,4
+
+
 main : Program Never Model Msg
 main =
     Html.program
         { init = init
         , view = view
         , update = update
-        , subscriptions = subscriptions
+        , subscriptions = always Sub.none
         }
 
-type Scale = Large | Medium | Small
-
-type alias PositionedMovie =
-    { movie : Movie,
-      scale : Scale }
 
 type alias Model =
-    {}
+    { layout : Layout }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}
+    ( { layout =
+            { frames = frames
+            , movies =
+                [ { fileName = "IMG_6250.m4v", orientation = Vertical }
+                , { fileName = "IMG_6227.m4v", orientation = Horizontal }
+                , { fileName = "IMG_6230.m4v", orientation = Horizontal }
+                , { fileName = "IMG_6231.m4v", orientation = Vertical }
+                ]
+            }
+      }
     , Cmd.none
     )
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
 
 
 type Msg
@@ -71,12 +112,33 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update action model =
     ( model, Cmd.none )
 
-movieView : Movie -> Html Msg
-movieView movie =
-  video [(loop True), (height 50), (src ("/public/" ++movie.fileName)), (autoplay  True)] []
+
+w : Frame -> String
+w f =
+    case f.orientation of
+        Vertical ->
+            "20%"
+
+        Horizontal ->
+            "30%"
+
+
+movieView : ( Movie, Frame ) -> Html Msg
+movieView ( movie, frame ) =
+    video
+        [ (loop True)
+        , (style
+            [ ( "width", w frame )
+            ]
+          )
+        , (src ("/public/" ++ movie.fileName))
+        , (autoplay True)
+        ]
+        []
+
 
 view : Model -> Html Msg
 view model =
     div
         []
-        (List.map movieView (take 6 movies))
+        (map movieView (zip model.layout.movies model.layout.frames))
